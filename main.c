220e40b910495d1a3e9198f317b9185feadae4ae
Vipul Verma <66062033+vip02aug@users.noreply.github.com>
2024-10-30 19:39:07 +0530

    GPIO init

--- main.c
+++ main.c
@@ -9,4 +9,20 @@
 #define CLKINT (1 << 2)                                   // SPECIFY CPU CLOCK
 #define CLOCK_HZ 16000000                                 // CLOCK FREQUENCY
 #define SYSTICK_RELOAD_VALUE(us) ((CLOCK_HZ / 1000000) * (us) - 1)   // SYSTICK RELOAD VALUE CONVERSION IN MICROSECONDS
+void I2C0_init(void) {
+    SYSCTL_RCGCI2C_R |= 0x01;                             // I2C0 CLOCK ENABLE
+    SYSCTL_RCGCGPIO_R |= 0x02;                            // GPIO PORTB CLOCK ENABLE
 
+    while ((SYSCTL_PRGPIO_R & 0x02) == 0)
+    {                                                     // DELAY TO ENSURE CLOCK IS STABILIZED
+    };
+
+    GPIO_PORTB_AFSEL_R |= 0x0C;                           // ENABLE ALERNATE FUNCTION FOR PB2 AND PB3
+    GPIO_PORTB_ODR_R |= 0x08;                             // ENABLE OPEN DRAIN ON SDA
+    GPIO_PORTB_DEN_R |= 0x0C;                             // ENABLE DIGITAL FOR PB2 AND PB3
+    GPIO_PORTB_PCTL_R &= ~0xFF00;                         // CLEAR PCTL
+    GPIO_PORTB_PCTL_R |= 0x3300;                          // I2C FUNCTIONALITY FOR PB2 AND PB3
+
+    I2C0_MCR_R = 0x10;                                    // I2C MASTER FUNCTION INITIALIZE
+    I2C0_MTPR_R = 0x07;                                   // SET CLOCK
+}
