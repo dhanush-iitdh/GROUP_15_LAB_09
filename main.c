d6c980204c21461de7f33230fab4dd8072458b21
Vipul Verma <66062033+vip02aug@users.noreply.github.com>
2024-10-30 19:41:33 +0530

    SYSTICK

--- main.c
+++ main.c
@@ -39,3 +39,9 @@ void I2C0_SEND(uint8_t PERIPHERAL_ADDRESS, uint8_t ANALOG_SAMPLE_MSB, uint8_t AN
     while (I2C0_MCS_R & 0x01);                            // WAIT FOR TRANSMISSION TO FINISH
     if (I2C0_MCS_R & 0x02) return;                        // RETURN IF ERROR OCCURS
 }
+void systick_setting(void)
+{
+    STRELOAD = SYSTICK_RELOAD_VALUE(1000);                // RELOAD VALUE FOR 1mS
+    STCTRL |= ENABLE | CLKINT;                            // ENABLE SYSTICK WITH SYSTEM CLOCK
+    STCURRENT = 0;                                        // CLEAR STCURRENT REGUSTER
+}
